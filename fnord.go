e8bac79c4dbe97695bbcd274b63c0a9ed57dbeb9
Philiphil <Philiphil@users.noreply.github.com>
2017-03-10 16:36:39 +0100

    Adding erisian holydays

--- fnord.go
+++ fnord.go
@@ -16,7 +16,20 @@ type DTime struct{
 	Hour int
 	Minute int
 	Second int
+	Holyday string
 }
+var holydays = [...]DTime{
+		DTime{0,Chaos,5,0,0,0,0,"Mungday"},
+		DTime{0,Chaos,50,0,0,0,0,"Chaoflux"},
+		DTime{0,Chaos,60,0,0,0,0,"St. Tib's Day"},
+		DTime{0,Discord,5,0,0,0,0,"Mojoday"},
+		DTime{0,Discord,50,0,0,0,0,"Discoflux"},
+		DTime{0,Confusion,5,0,0,0,0,"Syaday"},
+		DTime{0,Confusion,50,0,0,0,0,"Confuflux"},
+		DTime{0,Bureaucracy,5,0,0,0,0,"Zaraday"},
+		DTime{0,Bureaucracy,50,0,0,0,0,"Bureflux"},
+		DTime{0,Aftermath,5,0,0,0,0,"Maladay"},
+		DTime{0,Aftermath,50,0,0,0,0,"Afflux"}	}
 
 type DSeason int
 const (
@@ -61,9 +74,23 @@ func TimeToDTime(t time.Time) (f DTime){
 	d := t.YearDay()
 	f.Season = DSeason(d/SL)
 	f.Day = d%SL
-	f.DayN = DDay( (f.Day%5)-1)
+	if f.Day % 5 > 0 {
+		f.DayN = DDay( (f.Day%5)-1)
+	}else{
+		f.DayN = DDay( (f.Day%5))
+	}
 	f.Hour= t.Hour()
 	f.Minute= t.Minute()
 	f.Second= t.Second()
+	for _,v := range holydays{
+		if v.Season == f.Season && f.Day == v.Day{
+			if v.Day == 60 {
+				if t.Month() != 2{
+						continue
+				}
+			}
+			f.Holyday = v.Holyday
+		}
+	}
 	return
 }
